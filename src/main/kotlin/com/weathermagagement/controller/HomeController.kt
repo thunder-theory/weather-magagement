0b997134345fed8c557d1e1e7281426cc479a153
Bangseungjae <tmdwo5825@naver.com>
2022-11-18 00:29:25 +0900

    test controller

---
+++ src/main/kotlin/com/weathermagagement/controller/HomeController.kt
@@ -0,0 +1,11 @@
+package com.weathermagagement.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+
+    @GetMapping("/")
+    fun test() = "healthy"
+}
